58b5113dea5b69ceb58982d076ea19e98cfe730e
mnmr1996develop <93550664+mnmr1996develop@users.noreply.github.com>
2023-05-01 15:05:08 -0400

    getting up all the dependicies

--- app/src/main/java/com/michaelrichards/readertracker/MainActivity.kt
+++ app/src/main/java/com/michaelrichards/readertracker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.michaelrichards.readertracker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,20 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
 import com.michaelrichards.readertracker.ui.theme.ReaderTrackerTheme
+
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ReaderTrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
